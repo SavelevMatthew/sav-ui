d429f6c68fd36a35b823efc689184542d5d33e5a
Matthew Savelev <savelevmatthew@gmail.com>
2022-08-31 16:22:51 +0500

    ci: fixed style-dictionary

--- packages/ui/sd.config.js
+++ packages/ui/sd.config.js
@@ -1,8 +1,24 @@
+/**
+ * This file contains the configuration for the style-dictionary package,
+ * which converts figma-tokens into style variables for various platforms.
+ * Custom transformers are described in the bin/build-vars.js script, which uses this config
+ */
+
+const path = require('path')
+
 module.exports = {
-    source: ['src/tokens/sets/*.json'],
+    source: [path.join(__dirname, 'src/tokens/sets/*.json')],
     platforms: {
         css: {
-            transformGroup: 'css',
+            transforms: [
+                'name/cti/kebab',
+                'transformer/int-px',
+                'transformer/fonts/sans-serif',
+                'transformer/fonts/int-weight',
+                'transformer/shadow-css',
+                'transformer/short-hex',
+                'transformer/lower-hex',
+            ],
             prefix: 'condo',
             buildPath: 'src/tokens/',
             files: [
