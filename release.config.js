5fcb356a0c5c9f71513b7a5b3b9e8989f36352c8
Matthew Savelev <savelevmatthew@gmail.com>
2022-09-13 19:34:19 +0500

    fix(ui): change

--- release.config.js
+++ release.config.js
@@ -55,9 +55,7 @@ module.exports = {
     plugins: [
         ['@semantic-release/commit-analyzer', conventionalAnalyzerConfig],
         ['@semantic-release/release-notes-generator', conventionalChangelogConfig],
-        ['@semantic-release/npm', {
-            npmPublish: false,
-        }],
-        '@semantic-release/github',
+        ['@semantic-release/npm'],
+        // '@semantic-release/github',
     ],
 }
